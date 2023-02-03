ca17f786bb4c74b979bcf898cb1636ea36e64516
Adina Umbetova <adinaumbetova@gmail.com>
2023-02-04 02:03:42 +0600

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
-// You should implement your task here.
+
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+ 
+        let result = [];
+        let isForward = true;
+        for (let i = 0; i < matrix.length; i++) {
+            if (isForward) {
+                for (let j = 0; j < matrix[i].length; j++) {
+                    result.push(matrix[i][j]);
+                }
+                isForward = false;
+            } else {
+                for (let j = matrix[i].length - 1; j >= 0; j--) {
+                    result.push(matrix[i][j]);
+                }
+                isForward = true;
+            }
+        }
+        return result;
+    }
